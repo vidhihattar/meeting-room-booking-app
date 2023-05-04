b315edf400fb66010eda92d7658a58351cbd9d4e
ApoorvMishra1402 <apoorv.mishra.20cse@bmu.edu.in>
2023-05-04 19:22:33 +0530

    Mom live and momPad updates

--- client/src/components/MomLivePad.jsx
+++ client/src/components/MomLivePad.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 
-function MomLivePad() {
+function MomLivePad({meeting}) {
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState([]);
 
@@ -19,7 +19,7 @@ function MomLivePad() {
   return (
     <div className="notepad">
       <div className="notepad-header">
-        <h2>MOM:</h2>
+        <h2></h2>
       </div>
       <div className="notepad-form">
         <input
@@ -30,7 +30,10 @@ function MomLivePad() {
           onChange={handleInputChange}
         />
         <button className="notepad-add-btn" onClick={handleAddItem}>
-          Add
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.85714 16C7.30486 16 6.85714 15.5523 6.85714 15V10.1429C6.85714 9.59057 6.40943 9.14286 5.85714 9.14286H0.999999C0.447715 9.14286 0 8.69514 0 8.14286V7.85714C0 7.30486 0.447715 6.85714 1 6.85714H5.85714C6.40943 6.85714 6.85714 6.40943 6.85714 5.85714V0.999999C6.85714 0.447715 7.30486 0 7.85714 0H8.14286C8.69514 0 9.14286 0.447715 9.14286 1V5.85714C9.14286 6.40943 9.59057 6.85714 10.1429 6.85714H15C15.5523 6.85714 16 7.30486 16 7.85714V8.14286C16 8.69514 15.5523 9.14286 15 9.14286H10.1429C9.59057 9.14286 9.14286 9.59057 9.14286 10.1429V15C9.14286 15.5523 8.69514 16 8.14286 16H7.85714Z" fill="#442B48"/>
+</svg>
+
         </button>
       </div>
       <ul className="notepad-list">
--- client/src/pages/MomLive.jsx
+++ client/src/pages/MomLive.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NotePad from '../components/MomLivePad';
 import MomLivePad from '../components/MomLivePad';
+import { useMeetingsContext } from "../hooks/useMeetingsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useEffect } from "react";
 
 
 const MomLive = () => {
+    // const {meet, setMeet} = useState('');
+    const { user } = useAuthContext()
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const meetingId = urlSearchParams.get('meetingid');
+
+    var meeting = {}
+    useEffect(() => {
+        const fetchMeeting = async () => {
+          const response = await fetch(`/api/meetings/${meetingId}`, {
+            headers: { 'Authorization': `Bearer ${user.token}` }
+          });
+    
+          const json = await response.json();
+    
+          if (response.ok) {
+            meeting = json;
+            console.log(json);
+            console.log("response is ok");
+    
+          }
+        }
+    
+    
+         fetchMeeting()
+      }, [meeting]);
+     
   return (
+    <div className="right-container">
+            <div className="create-meeting-form-container">
+                <div className="create-meeting-heading">MoM Creation</div>
     <div className="mom-live">
        
       
-      <MomLivePad/>
+      <MomLivePad meeting={meeting}/>
     </div>
+</div>
+</div>
   );
 };
 
